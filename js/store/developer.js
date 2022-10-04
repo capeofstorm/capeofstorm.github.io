e974d978d82a9287ffa8e9c55cacb25f3877f0ae
Shadowsong <ombrechanson@outlook.com>
2022-10-04 21:43:20 +0800

    update

---
+++ js/store/developer.js
@@ -0,0 +1,32 @@
+let developer = [
+    {
+        url: 'https://github.com/zhangao204',
+        username: '影歌Shadowsong',
+        nick: '',
+        avatar: 'https://avatars.githubusercontent.com/u/109642163?v=4',
+        bio: 'Node.js开发(express.js，koa.js，NestJS)'
+    },
+    {
+        url: 'https://github.com/wanghaoyi1999',
+        username: '临江仙',
+        nick: '',
+        avatar: 'https://avatars.githubusercontent.com/u/67174942?v=4',
+        bio: '这个人很神秘'
+    },
+    {
+        url: 'https://github.com/haiwinds',
+        username: 'haiwinds',
+        nick: '',
+        avatar: 'https://avatars.githubusercontent.com/u/97596376?v=4',
+        bio: '漫不经心'
+    },
+    {
+        url: 'https://github.com/xuehaowei',
+        username: '劝君莫忆',
+        nick: '',
+        avatar: 'https://avatars.githubusercontent.com/u/112641440?v=4',
+        bio: '这个人很神秘'
+    }
+]
+
+export default developer
---
+++ js/store/project.js
@@ -0,0 +1,10 @@
+let project = [
+    {
+        url: '#',
+        name: '未公开',
+        summary: '敬请期待...',
+        imgUrl: 'https://pic1.imgdb.cn/item/61975a452ab3f51d91d3352f.jpg'
+    }
+]
+
+export default project
